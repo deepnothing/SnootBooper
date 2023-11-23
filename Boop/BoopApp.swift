18eb1f7fd2a3b25e96c1cf2e6d895c5939bee65b
teo <teo@nudgecoach.com>
2023-11-23 16:56:34 -0500

    Initial Commit

---
+++ Boop/BoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoopApp.swift
+//  Boop
+//
+//  Created by Teo Udovcic on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
