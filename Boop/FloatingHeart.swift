fb1e042fdc3fb8055d1952be96cf583f6d3bdafd
teo <teo@nudgecoach.com>
2024-01-05 17:26:59 -0500

    add game center entitlement

--- Boop/FloatingHeart.swift
+++ Boop/FloatingHeart.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-// Custom GeometryEffect for curved motion
+// custom GeometryEffect for curved motion
 struct CurvedMotionEffect: GeometryEffect {
     var amount: CGFloat
     var animatableData: CGFloat {
@@ -9,33 +9,33 @@ struct CurvedMotionEffect: GeometryEffect {
     }
     
     let shouldInvertXTranslation: Bool = Bool.random()
-
-
+    
+    
     func effectValue(size: CGSize) -> ProjectionTransform {
-        // Curve upwards with a slight horizontal deviation
+        // curve upwards with a slight horizontal deviation
         let baseXTranslation = sin(amount * .pi * 2) * 20
-
-       
-        // Flip the direction if shouldInvertXTranslation is true, this doent work yet because rendering a new
+        
+        
+        // flip the direction if shouldInvertXTranslation is true, this doent work yet because rendering a new
         // heart changes the path of a previous heart... can be replicted with the line below
         // let xTranslation = shouldInvertXTranslation ? -baseXTranslation : baseXTranslation
         let xTranslation = baseXTranslation
         let yTranslation = -100 * amount // Primary upward movement
-
+        
         return ProjectionTransform(CGAffineTransform(translationX: xTranslation, y: yTranslation))
     }
 }
 
-// FloatingHeart view
+// floatingHeart view
 struct FloatingHeart: View {
     @State private var motionAmount: CGFloat
     @State private var startAnimation = false
-
+    
     init() {
-        // Randomly determine the extent of horizontal deviation
+        // randomly determine the extent of horizontal deviation
         _motionAmount = State(initialValue: CGFloat.random(in: 0.5...1.5))
     }
-
+    
     var body: some View {
         Image(systemName: "heart.fill")
             .foregroundColor(.pink)
